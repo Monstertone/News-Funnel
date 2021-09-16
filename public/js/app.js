38c020a476a44849c679e28c215816baa70d192a
Monstertone <bmovie_1999@yahoo.com>
2021-09-15 21:54:28 -0700

    initial commit.   News portion mostly working here.  Needs some styling.

---
+++ public/js/app.js
@@ -0,0 +1,1 @@
+tempImg = storie.img;
---
+++ stories.js
@@ -0,0 +1,219 @@
+const story = 
+{
+  status: 'ok',
+  totalResults: 451,
+  articles: [
+    {
+      source: [Object],
+      author: 'Satvik Khare',
+      title: 'Vivo X70 Series Key Specifications Tipped via China Telecom Listings: Report - Gadgets 360',
+      description: "Vivo X70 series' key specifications have reportedly been spotted on China Telecom website, ahead of their launch on September 9. The flagship series has three smartphones — the vanilla Vivo X70, Vivo X70 Pro, Vivo X70 Pro+. A couple of hands-on videos have al…",
+      url: 'https://gadgets.ndtv.com/mobiles/news/vivo-x70-pro-plus-specifications-exynos-1080-snapdragon-888-soc-leak-china-telecom-launch-september-9-2533107',
+      urlToImage: 'https://i.gadgets360cdn.com/large/vivo_x70_pro_front_back_render_ithome_1631024767977.jpg',       
+      publishedAt: '2021-09-07T14:32:19Z',
+      content: "Vivo X70 series specifications have allegedly been leaked by China Telecom, as per a report, ahead of the smartphones' launch on September 9. The flagship Vivo smartphone series is expected to compri… [+3028 chars]"   
+    },
+    {
+      source: [Object],
+      author: null,
+      title: 'Augmented reality collection by Ukrainian brand hits runway - TODAYonline',
+      description: '(Refiles with no change to text)',
+      url: 'https://www.todayonline.com/world/augmented-reality-collection-ukrainian-brand-hits-runway',
+      urlToImage: 'https://www.todayonline.com/sites/all/themes/weekend/images/defaultimage.jpg',
+      publishedAt: '2021-09-07T14:30:01Z',
+      content: null
+    },
+    {
+      source: [Object],
+      author: null,
+      title: 'Today’s coronavirus news: Ontario reports 581 new COVID cases on Monday and 564 new cases on Tuesday; Ontario students head back to school amid fourth wave concerns - Toronto Star',
+      description: 'Meanwhile, the southern Indian state of Kerala is quickly ramping up efforts to stop a potential outbreak of the deadly Nipah virus, even as the state...',
+      url: 'https://www.thestar.com/news/canada/2021/09/07/covid-19-coronavirus-updates-toronto-canada-september-7.html',
+      urlToImage: 'https://images.thestar.com/Vjw1A3vl1GWnzLxLFEfMgq_FFrk=/1200x800/smart/filters:cb(1631024583128)/https://www.thestar.com/content/dam/thestar/news/canada/2021/09/07/covid-19-coronavirus-updates-toronto-canada-september-7/afw_firstdayschool04.jpg',
+      publishedAt: '2021-09-07T14:26:15Z',
+      content: 'The latest coronavirus news from Canada and around the world Tuesday. This file will be updated throughout the day. Web links to longer stories if available.\r\n' +
+        '10:19 a.m. (updated) Ontario reports 581… [+4907 chars]'
+    },
+    {
+      source: [Object],
+      author: 'Tony Leen',
+      title: 'Ronan McCarthy steps down as Cork football manager - Irish Examiner',
+      description: '',
+      url: 'https://www.irishexaminer.com/sport/gaa/arid-40387241.html',
+      urlToImage: 'https://www.irishexaminer.com/cms_media/module_img/5275/2637794_8_googlediscover_2050998_1_.jpg', 
+      publishedAt: '2021-09-07T14:12:00Z',
+      content: 'Ronan McCarthy has stepped down as manager of the Cork senior footballers, Examiner Sport understands.The executive of Cork GAA has decided not to extend his term into the final season of their two-y… [+3610 chars]'   
+    },
+    {
+      source: [Object],
+      author: 'Agency Report',
+      title: 'Taliban denies introduction of restrictions on social media - Punch Newspapers',
+      description: 'Taliban Spokesman Suhail Shaheen on Tuesday said the Taliban movement had no plans to introduce any restrictions on social media in Afghanistan.',
+      url: 'https://punchng.com/taliban-denies-introduction-of-restrictions-on-social-media/',
+      urlToImage: 'https://cdn.punchng.com/wp-content/uploads/2021/08/17071622/000_9L89CJ.jpg',
+      publishedAt: '2021-09-07T14:10:03Z',
+      content: 'Taliban Spokesman Suhail Shaheen on Tuesday said the Taliban movement had no plans to introduce any restrictions on social media in Afghanistan.\r\n' +
+        'Shaheen said the Taliban believed in the freedom of s… [+490 chars]'
+    },
+    {
+      source: [Object],
+      author: 'Steven Petite',
+      title: 'Snag Far Cry 3 For Free On PC For A Limited Time - GameSpot',
+      description: 'Ahead of the release of Far Cry 6, Ubisoft is giving away Far Cry 3.',
+      url: 'https://www.gamespot.com/articles/snag-far-cry-3-for-free-on-pc-for-a-limited-time/1100-6495970/',       
+      urlToImage: 'https://www.gamespot.com/a/uploads/screen_kubrick/1595/15950357/3876273-farcry3.png',
+      publishedAt: '2021-09-07T14:09:52Z',
+      content: "Far Cry 3 is one of the best entries in Ubisoft's over-the-top FPS franchise. If you missed out when it launched back in 2012 or simply want to revisit it, you can grab Far Cry 3 for free at the Ubis… [+1244 chars]"   
+    },
+    {
+      source: [Object],
+      author: 'Jennifer Jett, Caroline Radnofsky, Matthew Mulligan, Reuters',
+      title: 'Women join protests on Kabul streets in defiance of Taliban rule - NBC News',
+      description: 'Hundreds of Afghans, many of them women, protested in Kabul, chanting slogans against neighboring Pakistan and expressing support for rebels in Panjshir.',
+      url: 'https://www.nbcnews.com/news/world/women-join-protests-kabul-streets-defiance-taliban-rule-n1278583',    
+      urlToImage: 'https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2021_36/3503982/210907-afghan-women-mb-0929.jpg',
+      publishedAt: '2021-09-07T14:05:48Z',
+      content: 'Hundreds of Afghans, many of them women, protested in Kabul on Tuesday, chanting slogans against neighboring Pakistan and expressing support for rebels in the last part of the country resisting Talib… [+4093 chars]'   
+    },
+    {
+      source: [Object],
+      author: 'https://www.facebook.com/bbcnews',
+      title: 'Afghanistan: Taliban fire warning shots at protest in Kabul - BBC News',
+      description: "Unrest grows as more Afghans demand women's rights and chant anti-Taliban and anti-Pakistan slogans.",
+      url: 'https://www.bbc.com/news/world-asia-58472978',
+      urlToImage: 'https://ichef.bbci.co.uk/news/1024/branded_news/5340/production/_120421312_talibanfiringbbc.jpg', 
+      publishedAt: '2021-09-07T14:04:47Z',
+      content: 'media captionTaliban members were filmed firing shots into the air to disperse protesters\r\n' +     
+        'The Taliban have fired warning shots to disperse the crowd at a large protest in the Afghan capital, Kabul.\r… [+3424 chars]'
+    },
+    {
+      source: [Object],
+      author: 'https://www.facebook.com/bbcnews',
+      title: 'Triple lock pension pledge suspended for one year - BBC News',
+      description: 'It follows government concern that a big rise in earnings would have meant pensions increasing by 8%.',
+      url: 'https://www.bbc.co.uk/news/business-58476547',
+      urlToImage: 'https://ichef.bbci.co.uk/news/1024/branded_news/127E8/production/_120425757_garden_gettyimages-160075082.jpg',
+      publishedAt: '2021-09-07T14:03:45Z',
+      content: 'The government has confirmed a one-year suspension of the "triple lock" formula for annual state pension increases.\r\n' +
+        'The move follows government concern that a big post-pandemic rise in average earni… [+2449 chars]'
+    },
+    {
+      source: [Object],
+      author: "Carl O'Brien",
+      title: 'Grade inflation drives CAO points to record highs for many college courses - The Irish Times',
+      description: 'Many students with more than 600 points miss out on preferred programmes',
+      url: 'https://www.irishtimes.com/news/education/grade-inflation-drives-cao-points-to-record-highs-for-many-college-courses-1.4667346',
+      urlToImage: 'https://www.irishtimes.com/image-creator/?id=1.4667345&origw=1440',
+      publishedAt: '2021-09-07T14:02:16Z',
+      content: 'CAO points for entry into many college courses have jumped to a record high, with some students failing to get into courses despite securing more than 600 points.\r\n' +
+        'About 20 college courses across Tri… [+3776 chars]'
+    },
+    {
+      source: [Object],
+      author: null,
+      title: 'Dengue cases in Delhi: Causes, symptoms, precautions and all you need to know - India Today',
+      description: 'Dengue is a mosquito-borne viral infection that is caused by the bite of an infected Aedes mosquito and generally spreads in the monsoon season. All you need to know.',
+      url: 'https://www.indiatoday.in/information/story/dengue-cases-in-delhi-causes-symptoms-precautions-and-all-you-need-to-know-1850256-2021-09-07',
+      urlToImage: 'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202109/Chikungunya-647x363.jpeg?C5h5wb2gMRKMHQshQj3CxsfndDCHzSKm',
+      publishedAt: '2021-09-07T13:59:00Z',
+      content: null
+    },
+    {
+      source: [Object],
+      author: 'Rajeev Syal',
+      title: 'Tory MPs urge Priti Patel to return migrants who attempt to enter UK illegally - The Guardian',        
+      description: 'Suggestion to home secretary condemned as breach of UN convention on human rights',
+      url: 'https://amp.theguardian.com/uk-news/2021/sep/07/tory-mps-urges-priti-patel-to-return-migrants-to-france',      urlToImage: null,
+      publishedAt: '2021-09-07T13:58:00Z',
+      content: 'Immigration and asylumSuggestion to home secretary condemned as breach of UN convention on human rights\r\n' +
+        'Conservative MPs have urged the home secretary, Priti Patel, to send back immediately anyone i… [+4690 chars]'
+    },
+    {
+      source: [Object],
+      author: 'Adele Ankers',
+      title: "Phoebe Waller-Bridge Exits Donald Glover's Mr. and Mrs. Smith Series - IGN",
+      description: "Phoebe Waller-Bridge has reportedly exited Amazon's Mr. and Mrs. Smith series due to creative differences with Donald Glover.",
+      url: 'https://www.ign.com/articles/phoebe-waller-bridge-exits-mr-and-mrs-smith-amazon-series-donald-glover',   
+      urlToImage: 'https://assets-prd.ignimgs.com/2021/09/07/phoebe-waller-bridge-mr-and-mrs-smith-1631018498049.jpeg?width=1280',
+      publishedAt: '2021-09-07T13:52:40Z',
+      content: "Phoebe Waller-Bridge will no longer be working on Amazon's Mr. and Mrs. Smith series with Donald Glover, who remains attached as co-creator, executive producer and star.\r\n" +
+        'According to The Hollywood R… [+1830 chars]'
+    },
+    {
+      source: [Object],
+      author: 'Nithya P Nair',
+      title: 'Realme Dizo Watch 2, Dizo Watch Pro India Launch Set for September 15 - Gadgets 360',
+      description: 'Realme sub-brand Dizo will expand its smartwatch portfolio later this month with two new wearables — Realme Dizo Watch 2 and Realme Dizo Watch Pro. Realme Dizo Watch 2 will have a 1.69-inch display, while the Dizo Watch Pro will offer heart rate and blood oxy…',
+      url: 'https://gadgets.ndtv.com/wearables/news/realme-dizo-watch-2-pro-launch-price-in-india-specifications-features-september-15-2532957',
+      urlToImage: 'https://i.gadgets360cdn.com/large/Realme_Dizo_Watch_2_1631020210060.jpeg',
+      publishedAt: '2021-09-07T13:49:24Z',
+      content: 'Realme Dizo Watch 2 and Realme Dizo Watch Pro are set to launch in India on September 15 at 12pm IST at a virtual event. The Realme sub-brand launched the vanilla Dizo Watch in the country in August,… [+1995 chars]'   
+    },
+    {
+      source: [Object],
+      author: null,
+      title: "Researchers develop engineered 'mini' CRISPR genome editing system - ANI News",
+      description: "California [US], September 7 (ANI): CRISPR gene editing's common analogy is that it works like molecular scissors, cutting out select sections of DNA. Now, assistant professor of bioengineering at Stanford University, Stanley Qi, though likes that analogy, th…",
+      url: 'https://www.aninews.in/news/science/researchers-develop-engineered-mini-crispr-genome-editing-system20210907191021',
+      urlToImage: 'https://aniportalimages.s3.amazonaws.com/media/details/__sized__/vkdsvdhdv_PPsQyf0_0lUPWuV_297Qq5C20210907124816-thumbnail-154x87-70.jpg',
+      publishedAt: '2021-09-07T13:40:00Z',
+      content: 'ANI | Updated: Sep 07, 2021 19:10 IST\r\n' +
+        "California [US], September 7 (ANI): CRISPR gene editing's common analogy is that it works like molecular scissors, cutting out select sections of DNA. Now, assi… [+5018 chars]"
+    },
+    {
+      source: [Object],
+      author: null,
+      title: 'I apologise to my husband, Nigerians – BBNaija Tega - P.M. News',
+      description: 'Tega Dominic-Ajeboh, one of the recently evicted housemates of BBNaija, tenders a public apology to her husband, viewers of the show.',
+      url: 'https://pmnewsnigeria.com/2021/09/07/i-apologise-to-my-husband-nigerians-bbnaija-tega/',
+      urlToImage: 'https://cdn.pmnewsnigeria.com/wp-content/uploads/2021/09/596D5746-690E-4EAC-A5B9-E29E4AFCA3AF.jpeg',
+      publishedAt: '2021-09-07T13:36:30Z',
+      content: 'Tega Dominic-Ajeboh, one of the recently evicted housemates of BBNaija, has tendered a public apology to her husband, viewers of the show and Nigerians in general.\r\n' +
+        'She apologised while speaking in a… [+668 chars]'
+    },
+    {
+      source: [Object],
+      author: 'Tyler McCarthy',
+      title: "Monica Lewinsky says Bill Clinton should want to apologize for infamous affair, says she's moved on regardless - Fox News",
+      description: 'Monica Lewinsky got candid about her feelings toward Bill Clinton ahead of the premiere of the dramatization of their affair in “Impeachment: American Crime Story.”',
+      url: 'https://www.foxnews.com/entertainment/monica-lewinsky-bill-clinton-apologize-affair-moved-on',
+      urlToImage: 'https://static.foxnews.com/foxnews.com/content/uploads/2021/05/Monica-Lewinsky.jpg',
+      publishedAt: '2021-09-07T13:36:13Z',
+      content: 'Monica Lewinsky got candid about her feelings toward Bill Clinton ahead of the premiere of the dramatization of their affair in "Impeachment: American Crime Story." \r\n' +
+        'The former White House intern fo… [+2756 chars]'
+    },
+    {
+      source: [Object],
+      author: null,
+      title: 'Some Google Pixel 3, 3 XL phones are bricking without warning - MobileSyrup',
+      description: "According to reports, some Pixel 3 and 3 XL phones enter an 'EDL' mode and become completely unresponsive, with no way to recover the device.",
+      url: 'https://mobilesyrup.com/2021/09/07/google-pixel-3-xl-brick-no-warning-qualcomm-edl/',
+      urlToImage: 'https://cdn.mobilesyrup.com/wp-content/uploads/2021/09/google-pixel-3-header-20210907-scaled.jpg',      publishedAt: '2021-09-07T13:29:00Z',
+      content: 'Several Pixel 3 and 3 XL owners report an issue that renders their phones useless seemingly overnight.\r\n' +
+        'Posts on Google’s Issue Tracker webpage and support forums, as well as on Reddit, describe phon… [+1633 chars]'
+    },
+    {
+      source: [Object],
+      author: null,
+      title: 'NASA confirms Perseverance Mars rover got its first piece of rock - Guardian Nigeria',
+      description: 'NASA confirmed Monday that its Perseverance Mars rover succeeded in collecting its first rock sample for scientists to pore over when a future mission eventually brings it back to Earth.',
+      url: 'https://guardian.ng/features/nasa-confirms-perseverance-mars-rover-got-its-first-piece-of-rock/',        
+      urlToImage: 'https://guardian.ng/wp-content/uploads/2021/09/Mars.jpg',
+      publishedAt: '2021-09-07T13:21:10Z',
+      content: '(FILES) In this file NASA photo obtained July 25, 2021, 2021 shows NASA’s Ingenuity Mars Helicopter(R) captured by Mars Perseverance rover using its Left Mastcam-Z Camera, composed of a pair of camer… [+3335 chars]'   
+    },
+    {
+      source: [Object],
+      author: 'hindustantimes.com',
+      title: 'Kerala lifts night curfew, Sunday lockdown; Colleges to reopen from Oct 4 - Hindustan Times',
+      description: 'The decision was taken after a review meeting to assess the prevailing Covid-19 situation in the state. | Latest News India',
+      url: 'https://www.hindustantimes.com/india-news/kerala-lifts-night-curfew-sunday-lockdown-colleges-to-reopen-from-oct-4-101631019600031.html',
+      urlToImage: 'https://images.hindustantimes.com/img/2021/09/07/1600x900/df5ff7b4-0d96-11ec-b06b-bc6a184520fc_1631020468062_1631020476871.jpg',
+      publishedAt: '2021-09-07T13:19:38Z',
+      content: "Even as Kerala continues to majorly contribute to India's coronavirus disease (Covid-19) numbers, the state government on Tuesday decided to lift the night curfew and Sunday lockdown. The decision wa… [+1304 chars]"   
+    }
+  ]
+}
+
+exports.story = story;
+
